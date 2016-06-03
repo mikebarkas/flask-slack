c659121322bea75d70159c5dcaa4fc85d985a0f9
Mike Barkas <mike@barkas.com>
2016-06-03 17:15:09 -0400

    Create function for listing channels.

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 
 slack_client = SlackClient(SLACK_TOKEN)
 
-
+def list_channels():
+    channels_call = slack_client.api_call("channels.list")
+    if channels_call.get('ok'):
+        return channels_calle['channels']
+    return None
