fa900ca69199b80c151d1bafb804b128191e7e46
Mike Barkas <mike@barkas.com>
2016-06-03 17:19:36 -0400

    Add main function to list channels.

--- app.py
+++ app.py
@@ -5,8 +5,19 @@ SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 
 slack_client = SlackClient(SLACK_TOKEN)
 
+
 def list_channels():
     channels_call = slack_client.api_call("channels.list")
     if channels_call.get('ok'):
         return channels_calle['channels']
     return None
+
+
+if __name__ == '__main__':
+    channels = list_channels()
+    if channels:
+        print("Channels: ")
+        for c in channels:
+            print(c['name'] + " (" + c['id'] + ")")
+    else:
+        print("Unable to authenticate.")
