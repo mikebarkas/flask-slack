01c60a34367abdf0e6ec29b69ce1ae1c9b837945
Mike Barkas <mike@barkas.com>
2016-06-03 17:51:56 -0400

    Adding latest message to channel list output.

--- app.py
+++ app.py
@@ -13,11 +13,21 @@ def list_channels():
     return None
 
 
+def channel_info(channel_id):
+    channel_info = slack_client.api_call("channels.info", channel=channel_id)
+    if channel_info:
+        return channel_info['channel']
+    return None
+
+
 if __name__ == '__main__':
     channels = list_channels()
     if channels:
         print("Channels: ")
         for c in channels:
             print(c['name'] + " (" + c['id'] + ")")
+            channel_latest = channel_info(c['id'])
+            if channel_latest:
+                print(channel_latest['latest']['text'])
     else:
         print("Unable to authenticate.")
